da769cc060194d4ff15b4e7d7b54be2b7681ab31
beig perelman <berale6p@gmail.com>
2023-02-17 14:53:13 +0200

    feat(auth): create basic auth with google auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,GoogleStrategy],
 })
 export class AppModule {}
